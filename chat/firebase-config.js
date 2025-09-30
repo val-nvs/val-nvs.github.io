72d690917145d320235fe4d8dcdf6fc666c11c43
val-nvs <153785603+val-nvs@users.noreply.github.com>
2025-09-30 19:21:54 +0100

    edit

---
+++ chat/firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCxC1qidadTm5uLacKtD9d_6Zw1uq0Mnf0",
+    authDomain: "chat-8a799.firebaseapp.com",
+    databaseURL: "https://chat-8a799-default-rtdb.firebaseio.com",
+    projectId: "chat-8a799",
+    storageBucket: "chat-8a799.firebasestorage.app",
+    messagingSenderId: "359690574077",
+    appId: "1:359690574077:web:65ed1801a082314a8a8694"
+};
